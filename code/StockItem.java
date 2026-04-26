de781002b1c71c9d966872cd03af59d36ee8b91a
Will Tuffrey <william.tuffrey@yahoo.com>
2026-04-26 23:33:08 +0100

    v1.0.2-alpha
    Fixed StockItem.java class.

--- code/StockItem.java
+++ code/StockItem.java
@@ -1,4 +1,8 @@
 package code;
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class StockItem {
     private String stockCode;
@@ -9,41 +13,23 @@ public class StockItem {
         quantityInStock = Quantity;
         price = Price;
     }
-    public String GetStockName() {
-        return "Unknown Stock Name";
-    }
-    public String GetStockDescription() {
-        return "Unknown Stock Description";
-    }
-    public double GetVAT() {
-        return 0.2;
-    }
-    public String GetStockCode() {
-        return stockCode;
-    }
-    public void SetStockCode(String setCode) {
-        stockCode = setCode;
-    }
-    public int GetQuantityInStock() {
-        return quantityInStock;
-    }
-    public void SetQuantityInStock(int setQuantity) {
-        quantityInStock = setQuantity;
-    }
-    public double GetPrice() {
-        return price;
-    }
-    public void SetPrice(double setPrice) {
-        price = setPrice;
-    }
+    public String GetStockName() { return "Unknown Stock Name"; }
+    public String GetStockDescription() { return "Unknown Stock Description"; }
+    public double GetVAT() { return 0.2; }
+    public String GetStockCode() { return stockCode; }
+    public void SetStockCode(String setCode) { stockCode = setCode; }
+    public int GetQuantityInStock() { return quantityInStock;}
+    public void SetQuantityInStock(int setQuantity) { quantityInStock = setQuantity; }
+    public double GetPrice() { return price; }
+    public void SetPrice(double setPrice) { price = setPrice; }
 
     public boolean AddStock(int amount) {
         if (amount < 1) {
-            // Display error: "Quantity must be greater than 0."
+            System.out.println("Quantity must be greater than 0."); // Displayed error msg - show in swing window eventually
             return false;
         }
         if (quantityInStock + amount > 100) {
-            // Display error: "Cannot buy as the total stock will exceed 100 units."
+            System.out.println("Stock will exceed the maximum limit of 100 units."); // Displayed error msg - show in swing window eventually
             return false;
         }
         quantityInStock += amount;
@@ -51,15 +37,15 @@ public class StockItem {
     }
     public boolean SellStock(int amount) {
         if (amount < 1) {
-            // Display error: "Quantity must be greater than 0."
+            System.out.println("Quantity must be greater than 0."); // Displayed error msg - show in swing window eventually
             return false;
         }
         if (amount > quantityInStock) {
-            // Display error: "There are not enough items in stock."
+            System.out.println("There are not enough items in stock."); // Displayed error msg - show in swing window eventually
             return false;
         }
         if (quantityInStock - amount < 0) {
-            // Display error: "Cannot sell as the total stock will be lower than 0 units."
+            System.out.println("Cannot sell as the total stock will be lower than 0 units."); // Displayed error msg - show in swing window eventually
             return false;
         }
         quantityInStock -= amount;
